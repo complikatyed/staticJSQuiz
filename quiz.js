46411853bd33ea67fcb0b080205fee23ef36f7ec
complikatyed <katye.russell@gmail.com>
2016-02-19 14:24:56 -0600

    Add number validation and restructure for click events in JS

--- quiz.js
+++ quiz.js
@@ -18,15 +18,47 @@ growButton.addEventListener("click", function() {
   verifyInputBeforeBuild();
 });
 
-// ------------ Function for catching ENTER key press ----------------//
+// // ------------ Event listener for catching ENTER key press on HEIGHT ----------------//
 
-var inputKeyUp = function(e) {
-  e.which = e.which || e.keyCode;
-  if(e.which === 13) {     // keyCode 13 is the Enter key
 
-    verifyInputBeforeBuild();
-  }
-}
+// var heightInput = document.getElementById("height");
+
+//   heightInput.addEventListener("click", function() {
+
+//   inputKeyUp();
+// });
+
+// // ------------ Event listener for catching ENTER key press on HEIGHT ----------------//
+
+
+// var charInput = document.getElementById("char");
+
+//   charInput.addEventListener("click", function() {
+
+//   inputKeyUp();
+// });
+
+// // ------------ Function for catching ENTER key press  --------------------------//
+
+// var inputKeyUp = function(e) {
+//   //e.which = e.which || e.keyCode;
+//   if(e.keyCode === 13) {     // keyCode 13 is the Enter key
+
+//     verifyInputBeforeBuild();
+//   }
+// }
+
+
+// ------------ Click on the CLEAR button ----------------//
+
+
+var charInput = document.getElementById("clr");
+
+  charInput.addEventListener("click", function() {
+
+   alert("You pushed the clear button");
+});
+
 
 
 // ------------ Function that actually builds the tree in the console -----//
@@ -44,12 +76,16 @@ var buildTree = function(height, char) {
 
 var verifyInputBeforeBuild = function() {
   if (heightRequest.value === "" || charRequest.value === "") {
-    alert("Both fields need a value before we can build your tree.")
+    alert("Both fields need a value before we can build your tree.");
+  }
+  else if(isNaN(heightRequest.value)) {
+    alert("Please enter a number for your desired tree height.");
   }
   else {
     buildTree(heightRequest.value, charRequest.value);
   }
 }
+
 
 
 

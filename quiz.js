9924d4b657f2982c10899bc2ff663e6b24fcf733
complikatyed <katye.russell@gmail.com>
2016-02-23 20:47:34 -0600

    Refactor to pass in object and complete Greg's bonus suggestions

--- quiz.js
+++ quiz.js
@@ -1,4 +1,3 @@
-
 // --- Set the height input box as a variable ---- //
 
 var heightInput = document.getElementById("height");
@@ -9,16 +8,36 @@ var heightInput = document.getElementById("height");
 var charInput = document.getElementById("char");
 
 
+// Create tree object
+
+
+
+tree = {
+
+height: "",
+char: ""
+
+};
+
+tree.height = heightInput.value;
+tree.char = charInput.value;
+
+
 // ------------- Click on the "Grow Tree" button -------------- //
 
 var growButton = document.getElementById("btn");
 
 growButton.addEventListener("click", function() {
 
-  verifyInputBeforeBuild();
+  tree.height = heightInput.value;
+  tree.char = charInput.value;
+
+  verifyInputBeforeBuild(tree);
+
 });
 
-// // ------------ Event listener for catching ENTER key press on HEIGHT ----------------//
+
+// ------------ Event listener for catching ENTER key press on HEIGHT ----------------//
 
 
   heightInput.addEventListener("keypress", function(event) {
@@ -59,26 +78,35 @@ var clearPage = function() {
 
 // ------------ Function that actually builds the tree in the console ---------------//
 
-var buildTree = function(height, char) {
+var buildTree = function(myObj) {
+  var height = myObj.height;
+  var char = myObj.char;
 
   for (var i = -1; height > 0; i++) {
 
     console.log(" ".repeat(height -=1) + char.repeat(i += 2));
+
       i -= 1;
   }
+  clearPage(myObj);
 }
 
 // ------------- Function that checks for input in both fields before building ------//
 
-var verifyInputBeforeBuild = function() {
-  if (heightInput.value === "" || charInput.value === "") {
+
+var verifyInputBeforeBuild = function(myObj) {
+
+  if (myObj.height === "" || myObj.char === "") {
     alert("Both fields need a value before we can build your tree.");
   }
-  else if(isNaN(heightInput.value)) {
+  else if(isNaN(myObj.height)) {
     alert("Please enter a number for your desired tree height.");
   }
+  else if(myObj.height.length !== 1) {
+    alert("Please enter a number from 1-9.");
+  }
   else {
-    buildTree(heightInput.value, charInput.value);
+    buildTree(myObj);
   }
 }
 
